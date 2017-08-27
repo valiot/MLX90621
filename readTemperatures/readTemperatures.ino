9466966bcfe00f4e606f223fb0034549cdc54752
Baruc <baruc.almaguer@gmail.com>
2017-08-27 17:15:11 -0500

    Modificado baudrate en Arduino (ahora 115k2), modificados prints por blink de LED

--- readTemperatures/readTemperatures.ino
+++ readTemperatures/readTemperatures.ino
@@ -27,14 +27,18 @@
 MLX90621 sensor; // create an instance of the Sensor class
 
 void setup(){ 
-  Serial.begin(19200);
-  Serial.println(F("trying to initialize sensor..."));
-  sensor.initialise (4); // start the thermo cam with 4 frames per second
-  Serial.println(F("sensor initialized!"));
+  Serial.begin(115200); //170ms a 19k2, 28ms a 115k2
+  //Parpadea LED al reiniciarse el arduino
+  pinMode(13, OUTPUT);
+  for (int i = 0; i < 10; i++) 
+    digitalWrite(13, !digitalRead(13));
+  digitalWrite(13, LOW);
+  //Serial.println(F("trying to initialize sensor..."));
+  sensor.initialise (8); // start the thermo cam with 4 frames per second
+  //Serial.println(F("sensor initialized!"));
 }
 void loop(){
   sensor.measure(true); //get new readings from the sensor
-  
   for(int y=0;y<4;y++){ //go through all the rows
     Serial.print("[");
     
